c289ccf44a9e3b9c64b779bacb7b27ecdeae29d7
Dmssnnn <129981482+Dmssnnn@users.noreply.github.com>
2023-06-14 10:35:28 +0400

    task

---
+++ script.js
@@ -0,0 +1,8 @@
+// 1. გამოიყენეთ ყველა  სტრინგის მეთოდი რაც აქ არის ჩამოთვლილი და დალოგეთ კონსოლში ყველა მნიშვნელობა, ისე როგორც ლექციაზე გავაკეთეთ
+
+let text = "dimitri, giorgi , vache ";
+let result = text.slice(8,15);
+let length = text.length;
+let replace = text.replace("dimitri", "aja-aja")
+console.log(replace)
+
